caf1d85999438385793f25036b5388a79e7fd866
John Leonardo <johnleonardo@csus.edu>
2020-04-30 00:18:40 -0700

    create producer.ts

---
+++ lib/producer.ts
@@ -0,0 +1,17 @@
+// import redis
+import * as redis from "redis";
+
+// create publisher
+const publisher: redis.RedisClient = redis.createClient();
+
+// delay between messages (in ms)
+const delay: number = 2000;
+
+// run every 'delay' ms
+setTimeout(function run(): void {
+    // publish random 10 byte message to #channel
+    publisher.publish("#channel", Math.random().toString(36).substring(2, 15));
+
+    // call again
+    setTimeout(run, delay);
+}, delay);
